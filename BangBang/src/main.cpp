c7cdcf7839f5abe7083d5b21bcdf98ebe3a541e1
chalf <hieucuteom@gmail.com>
2024-08-28 23:29:49 +0700

    Hello World

---
+++ BangBang/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+using namespace std;
+
+int main(int arg, char* args[])
+{
+	cout << "Hello World\n";
+	//system("pause"); // stop console from exiting immediately for Windows; or cin.get(); for all.
+	
+	return 0;
+}
